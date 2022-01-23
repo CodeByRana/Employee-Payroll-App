d682c09b6d8d81299ac8b664a56770981ad8ef6c
deepakphpweb <dipakrana2760@gmail.com>
2022-01-24 00:12:54 +0530

    Added - Set event listner on salary range

---
+++ js/FormValidation.js
@@ -0,0 +1,26 @@
+window.addEventListener('DOMContentLoaded', (event) => {
+    validateName();
+    validateSalary();
+});
+//validation for name
+function validateName(){
+    const name = document.querySelector('#name');
+    const textError = document.querySelector('.text-error')
+    name.addEventListener('input',function(){
+        const nameRegex = RegExp('^[A-Z]{1}[a-z]{2,}$');
+        if(nameRegex.test(name.value)){         
+            textError.textContent = "";
+        }else{
+            textError.textContent = "Name is invalid";
+        }
+    });
+}
+//validation for salary
+function validateSalary(){
+    const salary = document.querySelector("#salary");
+    const output = document.querySelector('.salary-output');
+    output.textContent = salary.value;
+    salary.addEventListener('input',function(){
+        output.textContent= salary.value;
+    });
+}
