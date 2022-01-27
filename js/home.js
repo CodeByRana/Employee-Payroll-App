9e0ff135a7612bbeb57a9ca6fa6438320e3a14b6
deepakphpweb <dipakrana2760@gmail.com>
2022-01-28 01:53:34 +0530

    Added - Display Employee Details in Tabular Format From JSON Object

--- js/home.js
+++ js/home.js
@@ -14,24 +14,83 @@ const createInnerHTML = () => {
     <th>Start Date</th>
     <th>Actions</th>
 </tr>`;
+let empPayrollData = createEmployeePayrollJSON()[0];
+console.log(empPayrollData);
     const innerHtml = `${headerHtml}
                 <tr>
                     <td>
-                        <img class="profile" alt="profile-images" src="../assets/profile-images/Ellipse -2.png">
+                        <img class="profile" alt="profile-images" src="${empPayrollData
+                        ._profilePic}">
                     </td>
-                    <td>Narayan Mahadevan</td>
-                    <td>Male</td>
+                    <td>${empPayrollData._name}</td>
+                    <td>${empPayrollData._gender}</td>
                     <td>
-                        <div class="dept-label">HR</div>
-                        <div class="dept-label">Finance</div>
+                        <div class="dept-label">${empPayrollData._department[0]}</div>
+                        <div class="dept-label">${empPayrollData._department[1]}</div>
                     </td>
-                    <td>3000000</td>
-                    <td>1 Nav 2022</td>
+                    <td>${empPayrollData._salary}</td>
+                    <td>${empPayrollData._startDate}</td>
                     <td>
-                        <img id="1" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
-                        <img id="1" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
+                        <img name="${empPayrollData._id}" onclick="remove(this)" alt="delete" src="../assets/icons/delete-black-18dp.svg">
+                        <img name="${empPayrollData._id}" onclick="update(this)" alt="edit" src="../assets/icons/create-black-18dp.svg">
                     </td>
                 </tr>
     `;
     document.querySelector('#display').innerHTML = innerHtml;
+}
+
+const createEmployeePayrollJSON = () => {
+    let empPayrollListLocal = [
+        {
+            "_id": new Date().getTime(),
+            "_name": "Mark",
+            "_gender": "male",
+            "_department": [
+              "Finance",
+              "Engineer"
+            ],
+            "_salary": "500000",
+            "_startDate": '28 jan 2022',
+            "_note": "All In One",
+            "_profilePic": "../assets/profile-images/Ellipse -3.png"
+          },
+          {
+            "_id": 2,
+            "_name": "Bill",
+            "_gender": "male",
+            "_department": [
+              "Engineering"
+            ],
+            "_salary": "500000",
+            "_startDate": "29 Oct 2019",
+            "_note": "Terrific Engineer",
+            "_profilePic": "../assets/profile-images/Ellipse -1.png"
+          },
+          {
+            "_id": 3,
+            "_name": "Keerthi",
+            "_gender": "female",
+            "_department": [
+              "Sales"
+            ],
+            "_salary": "400000",
+            "_startDate": "29 Oct 2019",
+            "_note": "",
+            "_profilePic": "../assets/profile-images/Ellipse -1.png"
+          },
+          {
+            "_id": 4, 
+            "_name": "Kavya",
+            "_profilePic": "../assets/profile-images/Ellipse -1.png",
+            "_gender": "female",
+            "_department": [
+              "Finance",
+              "Engineer"
+            ],
+            "_salary": "428100",
+            "_note": "                ",
+            "_startDate": '27 oct 2022'
+          }
+    ];
+    return empPayrollListLocal;
 }
