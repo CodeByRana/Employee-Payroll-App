0f4be38b26b72135525c6f25927c83e255e25dda
deepakphpweb <dipakrana2760@gmail.com>
2022-01-28 14:13:28 +0530

    Added - View Employee Details data from Local Storage

--- js/home.js
+++ js/home.js
@@ -1,12 +1,20 @@
+let empPayrollList;
 window.addEventListener('DOMContentLoaded', () => {
-    createInnerHtml();
+  empPayrollList = getEmployeePayrollDataFromStorage();
+  document.querySelector(".emp-count").textContent = empPayrollList.length;
+  createInnerHtml();
+  localStorage.removeItem('editEmp');
 });
+
+const getEmployeePayrollDataFromStorage = () =>{
+  return localStorage.getItem('employeePayrollList') ? JSON.parse(localStorage.getItem('employeePayrollList')) : [];
+}
 
 //Template literal ES6 feature
 const createInnerHtml = () => {
-  let empPayrollList = createEmployeePayrollJSON();
-  console.log(empPayrollList);
-    const headerHtml = `
+  //let empPayrollList = createEmployeePayrollJSON();
+ // console.log(empPayrollList);
+  const headerHtml = `
     <tr>
     <th></th>
     <th>Name</th>
@@ -16,14 +24,14 @@ const createInnerHtml = () => {
     <th>Start Date</th>
     <th>Actions</th>
 </tr>`;
-let innerHtml = `${headerHtml}`;
+  let innerHtml = `${headerHtml}`;
 
-for (const empPayrollData of empPayrollList){
-     innerHtml = `${headerHtml}
+  for (const empPayrollData of empPayrollList) {
+    innerHtml = `${headerHtml}
                 <tr>
                     <td>
                         <img class="profile" alt="profile-images" src="${empPayrollData
-                        ._profilePic}">
+        ._profilePic}">
                     </td>
                     <td>${empPayrollData._name}</td>
                     <td>${empPayrollData._gender}</td>
@@ -36,70 +44,70 @@ for (const empPayrollData of empPayrollList){
                     </td>
                 </tr>
     `;
-    }
-    document.querySelector('#display').innerHTML = innerHtml;
+  }
+  document.querySelector('#display').innerHTML = innerHtml;
 }
 
 const createEmployeePayrollJSON = () => {
-    let empPayrollListLocal = [
-        {
-            "_id": 1,
-            "_name": "Mark",
-            "_gender": "male",
-            "_department": [
-              "Finance",
-              "Engineer"
-            ],
-            "_salary": "500000",
-            "_startDate": '28 jan 2022',
-            "_note": "All In One",
-            "_profilePic": "../assets/profile-images/Ellipse -3.png"
-          },
-          {
-            "_id": 2,
-            "_name": "Bill",
-            "_gender": "male",
-            "_department": [
-              "Engineering"
-            ],
-            "_salary": "500000",
-            "_startDate": "29 Oct 2019",
-            "_note": "Terrific Engineer",
-            "_profilePic": "../assets/profile-images/Ellipse -1.png"
-          },
-          {
-            "_id": 3,
-            "_name": "Keerthi",
-            "_gender": "female",
-            "_department": [
-              "Sales"
-            ],
-            "_salary": "400000",
-            "_startDate": "29 Oct 2019",
-            "_note": "",
-            "_profilePic": "../assets/profile-images/Ellipse -1.png"
-          },
-          {
-            "_id": 4, 
-            "_name": "Kavya",
-            "_profilePic": "../assets/profile-images/Ellipse -1.png",
-            "_gender": "female",
-            "_department": [
-              "Finance",
-              "Engineer"
-            ],
-            "_salary": "428100",
-            "_note": "                ",
-            "_startDate": '27 oct 2022'
-          }
-    ];
-    return empPayrollListLocal;
+  let empPayrollListLocal = [
+    {
+      "_id": 1,
+      "_name": "Mark",
+      "_gender": "male",
+      "_department": [
+        "Finance",
+        "Engineer"
+      ],
+      "_salary": "500000",
+      "_startDate": '28 jan 2022',
+      "_note": "All In One",
+      "_profilePic": "../assets/profile-images/Ellipse -3.png"
+    },
+    {
+      "_id": 2,
+      "_name": "Bill",
+      "_gender": "male",
+      "_department": [
+        "Engineering"
+      ],
+      "_salary": "500000",
+      "_startDate": "29 Oct 2019",
+      "_note": "Terrific Engineer",
+      "_profilePic": "../assets/profile-images/Ellipse -1.png"
+    },
+    {
+      "_id": 3,
+      "_name": "Keerthi",
+      "_gender": "female",
+      "_department": [
+        "Sales"
+      ],
+      "_salary": "400000",
+      "_startDate": "29 Oct 2019",
+      "_note": "",
+      "_profilePic": "../assets/profile-images/Ellipse -1.png"
+    },
+    {
+      "_id": 4,
+      "_name": "Kavya",
+      "_profilePic": "../assets/profile-images/Ellipse -1.png",
+      "_gender": "female",
+      "_department": [
+        "Finance",
+        "Engineer"
+      ],
+      "_salary": "428100",
+      "_note": "                ",
+      "_startDate": '27 oct 2022'
+    }
+  ];
+  return empPayrollListLocal;
 }
 
-const getDeptHtml = (deptList) =>{
-    let deptHtml = '';
-    for(const dept of deptList){
-        deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
-    }
-    return deptHtml;
+const getDeptHtml = (deptList) => {
+  let deptHtml = '';
+  for (const dept of deptList) {
+    deptHtml = `${deptHtml} <div class='dept-label'>${dept}</div>`;
+  }
+  return deptHtml;
 }
